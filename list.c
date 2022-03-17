865563acdc2f03177a5a24268b6f17cff1c7c2be
Your Name <you@example.com>
2022-03-17 20:39:42 -0300

    new try

--- list.c
+++ list.c
@@ -120,7 +120,8 @@ void * popBack(List * list) {
 
 void * popCurrent(List * list) 
 {
-    list->current->prev = list->current->next;
+    list->current = list->current->next;
+    free(list->current);
 
     return list->current->data;
 }
